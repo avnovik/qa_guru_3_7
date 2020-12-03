b65ed97135a58c2ba3c09655b04dc2a5fc65e6dc
Aleksei <3900676@gmail.com>
2020-12-03 13:38:52 +0300

    added Yandex autotest

---
+++ src/test/java/tests/YandexSearchTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexSearchTest {
+
+    @Test
+    void yandexSearchTests() {
+
+        open("https://www.yandex.ru/");
+        $("#text").val("Selenide").pressEnter();
+        $("body").shouldHave(text("selenide.org"));
+    }
+}
